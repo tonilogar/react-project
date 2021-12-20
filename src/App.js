bd388e40a87e3926af9d8f6f1c5990f7c7a440a4
pcoticgc <tologar@gmail.com>
2021-12-20 09:54:23 +0100

    copy files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ToolsOpen } from './components/ToolsOpen'
+import { ToolsClose } from './components/ToolsClose'
+/* import './App.css'; */
+
+function App() {
+  return (
+    <React.Fragment>
+      <ToolsOpen/>
+      <ToolsClose/>
+    </React.Fragment>
+  );
+}
+
+export default App;
